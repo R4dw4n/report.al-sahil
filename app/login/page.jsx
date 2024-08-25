c11f0a720e5c90033c819ca006fc489d71b9665e
Radwan Al-Kheder <3bodee.homs@gmail.com>
2024-08-25 18:35:36 +0300

    having login data inside the react function

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,49 @@
 'use client'
 import React from "react"
 import Login from "../components/Login/Login"
-import { loginData, loginTail } from "./data"
+// import { loginData, loginTail } from "./data"
 import logo from "../../public/logo_al_sahil2.png"
 import { useTranslation } from "react-i18next"
 
 const Page = () => {
   const { t } = useTranslation();
+  const loginData = [
+    {
+      formItemProps: {
+        name: "phone",
+        label: t("phone_number"),
+        rules: [
+          {required: true, message: "Please enter your phone number"},
+          {whitespace: true, message: "Phone number can't be empty"},
+        ]
+      },
+      input: {
+        type: "text",
+        className: inputClassName,
+      },
+    },
+    {
+      formItemProps: {
+        name: "password",
+        label: t("password"),
+        rules: [
+          {required: true, message: "Please enter your Password"},
+          {whitespace: true, message: "Password can't be empty"},
+        ]
+      },
+      input: {
+        type: "password",
+        className: inputClassName,
+      },
+    },
+  ];
+  
+  const loginTail = [
+    {
+      linkUrl: "/",
+      linkText: t("forgot_password") + "?"
+    }
+  ];
   return (
     <Login image={logo}
       paddingY="94"
